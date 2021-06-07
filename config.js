bb22b949215248f0d5445c137a542d3cdf6d9f81
duchuy123e <85492448+duchuy123e@users.noreply.github.com>
2021-06-07 19:40:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'hihi Lỡ yêu rồi mất khôn',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em. mà phải gọi là Anh trai nha <33',
     btnNo: 'Không, em trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Trời ơi ! iêu Huy nhất chứ còn ai vào đây nữa <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay có tính là xem đá bóng không đây ta. hay là ngủ cho mai học online',
-    btnAccept: 'chắt coi đá bóng nhỉ Indo tuổi tí <3',
+    btnAccept: 'chắt coi đá bóng nhỉ  <3',
     messLink: 'https://www.facebook.com/messages/t/100011528596614'
 }
